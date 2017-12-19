e809ef741cdf882247908721d873b2bf2ac714c7
Glitch (caring-nickel) <none>
2017-12-19 22:14:36 +0000

    🌋🚀 Checkpoint .env:575125 server.js:575125

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
+//build googlesearch
+var GoogleSearch = require('google-search');
+var googleSearch = new GoogleSearch({
+  key: process.env.GOOGLE_API_KEY,
+  cx: process.env.GOOGLE_CX
+});
 
+googleSearch.build({
+  q: "cat and deer",
+  start: 0,
+  fileType: "jpg",
+  num: 10, // Number of search results to return between 1 and 10, inclusive 
+  
+}, function(error, response) {
+  if (error){
+    console.log("Request Error: " + error);
+  }
+  console.log(response);
+
+})
 
 app.use(express.static('public'));
 
