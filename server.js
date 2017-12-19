98b62bca18406b7faadcbf444b85ec802efa6d03
Glitch (caring-nickel) <none>
2017-12-19 22:25:36 +0000

    ⛩📠 Checkpoint server.js:575125

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 var express = require('express');
 var app = express();
+var url = require('url');
 //build googlesearch
 var GoogleSearch = require('google-search');
 var googleSearch = new GoogleSearch({
   key: process.env.GOOGLE_API_KEY,
   cx: process.env.GOOGLE_CX
 });
-
+/*
 googleSearch.build({
-  q: "cat and deer",
-  start: 0,
-  fileType: "jpg",
+  q: "cat",
+  start: 5,
+  searchType: "image",
   num: 10, // Number of search results to return between 1 and 10, inclusive 
   
 }, function(error, response) {
   if (error){
-    console.log("Request Error: " + error);
+    console.log(error);
   }
   console.log(response);
 
 })
-
+*/
 app.use(express.static('public'));
 
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+  console.log(url.query)
 });
 
 
